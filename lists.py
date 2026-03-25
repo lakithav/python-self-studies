acbaf73abf18c3c65cf1c292fe8628cd2cee8cc6
lakitha21 <lakithaviraj21@gmail.com>
2026-03-25 08:40:02 +0530

    lists in python

---
+++ lists.py
@@ -0,0 +1,38 @@
+x = [1, 2, 3, 4, 5]
+y = ['hello', 'world', 'python', 'programming']
+z = [1, 'hello', 3.14, True, [1, 2, 3]]
+
+# Accessing elements
+
+'''
+print(x[0]) 
+print(z[4])
+print(z)
+'''
+#print(len(z))
+
+#x[1] = 20
+#print(x)
+
+'''
+x.insert(2,10)
+print(x)
+
+x.append(50)
+print(x)
+
+x.pop()
+print(x)
+
+z.clear()
+print(z)
+
+'''
+a  = [11,10,5,8,99]
+print(a)
+
+a.sort()
+print(a)
+
+a.sort(reverse=True)
+print(a)
